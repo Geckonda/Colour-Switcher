5afa28b748061441a11f097a91b3529e1b3463fd
geckonda <aquariusstd@yandex.ru>
2022-07-25 23:39:56 +0400

    v1.0

---
+++ index.js
@@ -0,0 +1,31 @@
+const R = document.getElementById('R-CL');
+const G = document.getElementById('G-CL');
+const B = document.getElementById('B-CL');
+const HEX = document.getElementById('HEX-CL')
+
+const currentColour = document.getElementById('current__Colour');
+const currentHEX = document.getElementById('current__HEX');
+const body =  document.body;
+
+console.log(body);
+
+R.addEventListener("change", ()=>{
+    ChangeColour();
+})
+G.addEventListener("change", ()=>{
+    ChangeColour();
+})
+B.addEventListener("change", ()=>{
+    ChangeColour();
+})
+HEX.addEventListener("change", ()=>{
+    ChanheWithHEX();
+})
+function ChangeColour(){
+    currentColour.style.background =  `rgb(${R.value},${G.value},${B.value})`;
+    currentHEX.innerHTML = currentColour.style.background;
+    body.style.background =  `rgb(${R.value},${G.value},${B.value})`;
+}
+function ChanheWithHEX(){
+    console.log(HEX.value);
+}
