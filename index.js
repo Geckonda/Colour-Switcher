9bbde64f843ef43c097bfb4a070b7ffa9389a139
geckonda <aquariusstd@yandex.ru>
2022-07-31 21:54:39 +0400

    Game

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const currentHEX = document.getElementById('current__HEX');
 const body =  document.body;
 
 const RGB = [R, G, B];
+var r,g,b;
 
 RGB.forEach(slider =>{
     slider.addEventListener("mousemove", ChangeColour);
@@ -18,4 +19,70 @@ function ChangeColour(){
     currentColour.style.background =  `rgb(${R.value},${G.value},${B.value})`;
     currentHEX.innerHTML = currentColour.style.background;
     body.style.background =  `rgb(${R.value},${G.value},${B.value})`;
+    ColourisEqual();
 }
+
+const randColour = document.getElementById('random__Colour');
+
+randColour.style.background = Random();
+console.log("Random colour = " + randColour.style.background);
+
+function Random(){
+    r = Math.floor(Math.random()*256);
+    g = Math.floor(Math.random()*256);
+    b = Math.floor(Math.random()*256);
+    return rgb =  `rgb(${r},${g},${b})`;
+}
+
+const x = 5;
+function ColourisEqual(){
+   if((R.value >= r-x && R.value <= r+x) && 
+    (G.value >= g-x && G.value <= g+x) &&
+    (B.value >= b-x) && (B.value <=b+x)){
+    ShowMarks();
+    GetHelp();
+    console.log("Success");
+    console.log("Random colour = " + randColour.style.background);
+   }
+}
+
+const hint = document.getElementById('hint');
+GetHelp();
+
+function GetHelp(){
+    setTimeout(() => {
+        hint.style.cursor = 'pointer';
+        hint.innerHTML = "CLICK to know!";
+        hint.addEventListener("click", ShowHint);
+    }, 10000);
+}
+
+function ShowHint(){
+        hint.style.cursor = 'text';
+        hint.innerHTML = randColour.style.background;
+        hint.removeEventListener("click", ShowHint);
+}
+
+const marks = document.getElementsByClassName('marks')
+function ShowMarks(){
+    let interval  = setInterval(() => {
+            let curOp = MarksOpacity();
+            marks[0].style.opacity = curOp
+            
+            marks[1].style.opacity = curOp
+    }, 250);
+    setTimeout(() => {
+        clearInterval(interval);
+        randColour.style.background = Random();
+        hint.innerHTML = "Try to guess!";
+    }, 1500);
+}
+var op = false;
+function MarksOpacity(){
+    op = !op;
+    if(!op)
+        return 0;
+    else
+        return 1;
+}
+
